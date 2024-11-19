e0410940549cc498fd34589220af06552860d4d2
KimJin <khkcejkms1@naver.com>
2024-11-19 23:12:54 +0900

    누락한거 있어서 다시올려요 24.11.19

---
+++ Test/Assets/Scripts/newScripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("테스트 중이지요~");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
